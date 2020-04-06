a58fb4edcc31b18b212b79aec946a32eea9c72b2
AlejaMC15 <aleja-mc@hotmail.com>
2020-04-05 21:42:34 -0500

    se modifico el diseño del cv

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import landingPage from '../images/landingpage.jpeg'
 import formulario from '../images/tributepage.jpeg'
 import tributePage from '../images/form.jpeg'
 import curriculum from '../CV/CV Alejandra Meneses Carmona.pdf'
+import imgCuv from '../images/imgCv.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './Styles/index.css'
 
@@ -12,15 +13,15 @@ class Main extends React.Component {
 
     render() {
 
-        const { imgAle, title, textOne, titleTwo, paragraphOne, titleThree, titleFourth} = this.props
+        const { imgAle, title, textOne, titleTwo, paragraphOne, titleThree, titleFourth } = this.props
 
         return (
 
             <div>
-                <div id="inicio" className="container" style={{'padding': '55px'}}>
+                <div id="inicio" className="container" style={{ 'padding': '55px' }}>
                     <div className="row">
                         <div className="col-md-6 col-md-offset-6">
-                            <img className="img-thumbnail img-responsive img-fluid" style={{'maxWidth': '100%', height: 'auto'}} src={pictureAle} />
+                            <img className="img-thumbnail img-responsive img-fluid" style={{ 'maxWidth': '100%', height: 'auto' }} src={pictureAle} />
                         </div>
                         <div className="col-md-6 col-md-offset-6 align-self-center align-items-center justify-content-justify">
                             <h2 className="titulo1">{title}</h2>
@@ -32,23 +33,23 @@ class Main extends React.Component {
                 <hr />
 
                 <div id="quien-soy" className="quien" className="container text-align-center justify-content-justify align-content-center flex-wrap"
-                    style={{'padding': '60px'}}>
-                    <h2 style={{'textAlign': 'center'}}>{titleTwo}</h2>
+                    style={{ 'padding': '60px' }}>
+                    <h2 style={{ 'textAlign': 'center' }}>{titleTwo}</h2>
                     <br />
-        <p className="texto2">{paragraphOne}</p>
+                    <p className="texto2">{paragraphOne}</p>
                 </div>
 
                 <hr />
 
                 <div>
-                    <h2 id="proyectos" className="titulo2" style={{'padding': '60px'}}>{titleThree}</h2>
+                    <h2 id="proyectos" className="titulo2" style={{ 'padding': '60px' }}>{titleThree}</h2>
                 </div>
-                    
+
                 <div id="cards" className="container-fluid center-block d-flex justify-content-around">
                     <div className="row">
                         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                            <div className="card" style={{'width': '18rem'}}>
-                                <img src={landingPage} className="card-img-top" alt="..." />
+                            <div className="card" style={{ 'width': '18rem' }}>
+                                <img src={landingPage} className="card-img-top" alt="..." style={{'height': '135px', 'padding': '5px'}} />
                                 <div className="card-body">
                                     <h5 className="card-title">Página de productos</h5>
                                     <p className="card-text">Mi tercer proyecto.</p>
@@ -57,8 +58,8 @@ class Main extends React.Component {
                             </div>
                         </div>
                         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                            <div className="card" style={{'width': '17.5rem', 'height': '18.5rem'}}>
-                                <img src={formulario} className="card-img-top" alt="..." />
+                            <div className="card" style={{ 'width': '17.5rem', 'height': '18.5rem' }}>
+                                <img src={formulario} className="card-img-top" alt="..." style={{'height': '135px', 'padding': '5px'}} />
                                 <div className="card-body">
                                     <h5 className="card-title">Formulario de encuesta</h5>
                                     <p className="card-text">Mi segundo proyecto.</p>
@@ -67,8 +68,8 @@ class Main extends React.Component {
                             </div>
                         </div>
                         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                            <div className="card" style={{'width': '18rem', 'height': '18.5rem'}}>
-                                <img src={tributePage} className="card-img-top" alt="..." />
+                            <div className="card" style={{ 'width': '18rem', 'height': '18.5rem'}}>
+                                <img src={tributePage} className="card-img-top" alt="..." style={{'height': '135px', 'padding': '5px'}} />
                                 <div className="card-body">
                                     <h5 className="card-title">Página tributo</h5>
                                     <p className="card-text">Mi primer proyecto</p>
@@ -80,13 +81,21 @@ class Main extends React.Component {
                 </div>
                 <hr />
                 <div>
-                    <h2 id="cv" style={{'textAlign': 'center', 'padding': '50px'}}>{titleFourth}</h2>
-                    <a href={curriculum} download="CV Alejandra Meneses Carmona">
-                    <h2 style={{'textAlign': 'center', 'padding': '50px', 'color': 'black'}}>Descargar</h2></a>
+                    <h2 id="cv" style={{ 'textAlign': 'center', 'padding': '50px' }}>{titleFourth}</h2>
+                    <h2 style={{ 'textAlign': 'center', 'padding': '50px', 'color': 'black' }}>Haga click en la imagen para descargar</h2>
+                    <div className="container-fluid center-block d-flex justify-content-around">
+                        <div className="row">
+                            <div className="col-md-2 col-md-offset-2">
+                                <a href={curriculum} download="CV Alejandra Meneses Carmona">
+                                    <img className="img-responsive" src={imgCuv} style={{'width': '800px', 'height': '500px','padding': '15px', 'background': 'black'}} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <hr />
             </div>
-            
+
         )
     }
 
